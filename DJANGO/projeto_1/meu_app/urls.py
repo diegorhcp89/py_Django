63ac9da5d3f0649b583556874cdc731e8cf47e1f
diegorhcp89 <diegoferreiradoamaral@gmail.com>
2025-08-05 15:51:47 -0300

    Mapeamento de URLs

--- DJANGO/projeto_1/meu_app/urls.py
+++ DJANGO/projeto_1/meu_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import home
+from .views import home, about, contact
 
 urlpatterns = [
     path("", home, name="home"),
+    path('sobre/', about, name="about"),
+    path('contato/', contact, name="contact")
 ]
--- DJANGO/projeto_1/meu_app/views.py
+++ DJANGO/projeto_1/meu_app/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return HttpResponse("Bem-vindo ao meu primeiro app Django!")
+
+def about(request):
+    return HttpResponse("Sobre nós - Infomações da empresa")
+
+def contact(request):
+    return HttpResponse("Entre em contato")
