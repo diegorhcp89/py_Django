cbc85e9360c7a6e24fa9dad0ec3b28b3b69e9871
diegorhcp89 <diegoferreiradoamaral@gmail.com>
2025-08-07 11:36:49 -0300

    Criando Models com Relacionamento

--- DJANGO/projeto_1/meu_app/admin.py
+++ DJANGO/projeto_1/meu_app/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Cliente
+from .models import Cliente, Categoria, Produto
 
 admin.site.register(Cliente)
+admin.site.register(Categoria)
+admin.site.register(Produto)
--- DJANGO/projeto_1/meu_app/models.py
+++ DJANGO/projeto_1/meu_app/models.py
@@ -8,3 +8,18 @@ class Cliente(models.Model):
 
     def __str__(self):
         return self.nome
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
+
+class Produto(models.Model):
+    nome = models.CharField(max_length=100)
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+    estoque = models.IntegerField()
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nome
