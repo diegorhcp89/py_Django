6ce20c5f7cda85cb0ea89195a17f59251e890501
diegorhcp89 <diegoferreiradoamaral@gmail.com>
2025-08-07 11:06:57 -0300

    Interagindo com a ORM do Django

--- DJANGO/projeto_1/meu_app/models.py
+++ DJANGO/projeto_1/meu_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Cliente(models.Model):
+    nome = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    idade = models.IntegerField()
+
+    def __str__(self):
+        return self.nome
